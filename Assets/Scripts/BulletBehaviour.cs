23196fddc64cbf909d4d9cd68e6988f5b47f531c
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-24 17:19:00 -0500

    Created a gun script with raycast (In progress)

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private GameObject gunTip;
-    [SerializeField]
-    private GameObject Bullet;
-    [SerializeField]
+    public GameObject gunTip;
+    private Rigidbody m_Rigidbody;
     private float speed = 20;
     void Start()
     {
-        transform.rotation = gunTip.transform.rotation;
+        m_Rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(Bullet);
-        Bullet.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        m_Rigidbody.velocity = transform.forward * speed * Time.deltaTime;
+        Destroy(this.gameObject, 5);
     }
 }
--- Assets/Scripts/GunBehaviour.cs
+++ Assets/Scripts/GunBehaviour.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GunBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private GameObject GunTip;
-    [SerializeField]
-    private GameObject Bullet;
-    void Start()
-    {
-        
-    }
+    public float damage = 1f;
+    public float range = 100f;
 
-    // Update is called once per frame
+    public GameObject gunTip;
+   
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gunTip.transform.position, gunTip.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
         
     }
 }
