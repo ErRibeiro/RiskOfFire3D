f8205651a3996caa0bb686c3e49eded2c860efb3
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-24 16:13:12 -0500

    Added a playable character with controls and working on a weapon

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject gunTip;
+    [SerializeField]
+    private GameObject Bullet;
+    [SerializeField]
+    private float speed = 20;
+    void Start()
+    {
+        transform.rotation = gunTip.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(Bullet);
+        Bullet.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
